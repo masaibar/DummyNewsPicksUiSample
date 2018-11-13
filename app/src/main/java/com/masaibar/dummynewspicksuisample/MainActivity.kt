fe10da64edeca2baaaf1beb85ce76979087a026f
masaibar <masaibar.dev@gmail.com>
2018-11-13 13:18:18 +0900

    Activityの骨組みまで

--- app/src/main/java/com/masaibar/dummynewspicksuisample/MainActivity.kt
+++ app/src/main/java/com/masaibar/dummynewspicksuisample/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.masaibar.dummynewspicksuisample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import com.masaibar.dummynewspicksuisample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this,
+            R.layout.activity_main
+        )
+
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
+
+
+        val toggle = object : ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            toolbar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        ) {
+        }
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 }
